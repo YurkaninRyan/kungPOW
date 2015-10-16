1b2d3ecafeae2f0885247f1ee0d5432d5ba4e6aa
Ryan Yurkanin <yurkaninryan@hotmail.com>
2015-10-15 20:18:01 -0400

    whatever

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-var client = require('twilio')('TWILIO_ACCOUNT_SID', 'TWILIO_AUTH_TOKEN');
+var client = require('twilio')(process.env.TWILIO_ACCOUNT_SID, process.env.TWILIO_AUTH_TOKEN),
+    app = require('express')(),
+    http = require('http').Server(app),
+    io = require('socket.io')(http)
+    port = process.env.PORT || 3000;
+
+app.get('/', function(req, res){
+  res.sendfile('index.html');
+});
+
+http.listen(port, function(){
+  console.log('listening on ' + port);
+});
