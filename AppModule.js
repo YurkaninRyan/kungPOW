a6894eab6e79382a016692e1d346ac427a6813ac
Vuk Petrovic <vukpetrovic1@yahoo.com>
2015-10-17 07:15:56 -0400

    Starting options menu work

--- AppModule.js
+++ AppModule.js
@@ -13,11 +13,24 @@ app.config(['$routeProvider', function($routeProvider) {
 }]);
 
 app.controller('AppController', function($scope) {
-	$scope.postList = [];
+	$scope.postList = []; $scope.optionsOpened = false;
 	$scope.glued = true;
+	$scope.inputPlaceholder = getPlaceholder();
 
 	$scope.getClass = function(post) {
 		return post.self ? "self" : "notSelf";
+	}
+
+	$scope.toggleOptions = function() {
+		var chatBox = document.getElementsByClassName('posts')[0];
+		if($scope.optionsOpened) {
+			//close options window
+			chatBox.style.height = "85%";
+		} else {
+			//open options window 
+			chatBox.style.height = "78%";
+		}
+		$scope.optionsOpened = !$scope.optionsOpened;
 	}
 
 	//function to add the post to the chat 
@@ -56,3 +69,7 @@ function addZero(i) {
     }
     return i;
 }
+
+var placeholders = [
+	"Example 1"
+]
