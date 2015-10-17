b710d44cdb23ec48c8b5e1767086d1a9af81f7a3
Vuk Petrovic <vukpetrovic1@yahoo.com>
2015-10-17 06:13:23 -0400

    Did so much stuff

--- AppModule.js
+++ AppModule.js
@@ -1,5 +1,5 @@
 
-var app = angular.module("kungPowApp", ['ngRoute']);
+var app = angular.module("kungPowApp", ['ngRoute', 'luegg.directives']);
 
 app.config(['$routeProvider', function($routeProvider) {
     $routeProvider
@@ -13,5 +13,46 @@ app.config(['$routeProvider', function($routeProvider) {
 }]);
 
 app.controller('AppController', function($scope) {
-	
+	$scope.postList = [];
+	$scope.glued = true;
+
+	$scope.getClass = function(post) {
+		return post.self ? "self" : "notSelf";
+	}
+
+	//function to add the post to the chat 
+	$scope.addPost = function() {
+		//holds all the data related to making a post
+		var postData = {
+			"username": "Vuk Petrovic",
+			"avatar": "man1.svg",
+			"content": "",
+			"self": true,  				//will be removed, and checks for self will be done via username
+			"blocked": false,
+			"starred": false,
+			"date": null
+		}
+
+		//add the post
+		postData.content = $scope.inputMessage;
+		var now = new Date();
+		postData.date = (now.getMonth() + 1) + "/" + now.getDate() + "/" + now.getFullYear() + " - " + 
+						(now.getHours() % 12) + ":" + addZero(now.getMinutes());
+		$scope.inputMessage = "";
+		$scope.postList.push(postData);
+	}	
 });
+
+//chat-box
+app.directive('chatBox', function() {
+	return {
+		'templateUrl': '/templates/chatbox.html'
+	}
+})
+
+function addZero(i) {
+    if (i < 10) {
+        i = "0" + i;
+    }
+    return i;
+}
